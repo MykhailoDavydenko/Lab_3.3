263b7424a402b7c90ba8cfe7c51296027e4f4753
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-10-02 18:23:53 +0300

    vrgerg

---
+++ Lab_03.3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Lab_03_3.cpp
+// Давиденко Михайло
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 7
+double y;
+Console.WriteLine("Введіть значення R: ");
+double R = double.Parse(Console.ReadLine());
+Console.WriteLine("Введіть значення x: ");
+double x = double.Parse(Console.ReadLine());
+if (x <= R)
+    y = R;
+else if (x >= -R && x <= R)
+    y = R - Math.Sqrt(R * R - x * x);
+else if (x >= R && x <= 6)
+    y = R + (-3 - R) / (6 - R) * (x - R);
+else
+    y = x-9;
+Console.WriteLine("y = "+y);
